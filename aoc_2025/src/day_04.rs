d4cc260438c613bb7222022c084fa1da753e86d2
Michael Conrad <mmitton@gmail.com>
2025-12-04 06:52:07 -0500

    2025 Day 4 - opt

--- aoc_2025/src/day_04.rs
+++ aoc_2025/src/day_04.rs
@@ -3,7 +3,7 @@ use helper::{Error, HashMap, HashSet, Lines, LinesOpt, print, println};
 
 #[derive(Default)]
 pub struct Day04 {
-    rolls: HashSet<(isize, isize)>,
+    rolls: Vec<Vec<bool>>,
 }
 
 impl Day04 {
@@ -11,26 +11,39 @@ impl Day04 {
         Self::default()
     }
 
-    fn can_remove(&self) -> Vec<(isize, isize)> {
-        let mut to_remove = Vec::new();
-        for (x, y) in self.rolls.iter() {
-            let mut adjacent = 0;
-            for (dx, dy) in &[
-                (-1, -1),
-                (0, -1),
-                (1, -1),
-                (-1, 0),
-                (1, 0),
-                (-1, 1),
-                (0, 1),
-                (1, 1),
-            ] {
-                if self.rolls.contains(&(x + dx, y + dy)) {
-                    adjacent += 1;
+    fn can_remove(&self, mut to_remove: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+        to_remove.clear();
+        for (y, row) in self
+            .rolls
+            .iter()
+            .enumerate()
+            .skip(1)
+            .take(self.rolls.len() - 2)
+        {
+            for (x, active) in row.iter().enumerate().skip(1).take(row.len() - 2) {
+                if !active {
+                    continue;
                 }
-            }
-            if adjacent < 4 {
-                to_remove.push((*x, *y));
+                let mut adjacent = 0;
+                for (dx, dy) in &[
+                    (-1, -1),
+                    (0, -1),
+                    (1, -1),
+                    (-1, 0),
+                    (1, 0),
+                    (-1, 1),
+                    (0, 1),
+                    (1, 1),
+                ] {
+                    let x1 = (x as i32 + dx) as usize;
+                    let y1 = (y as i32 + dy) as usize;
+                    if self.rolls[y1][x1] {
+                        adjacent += 1;
+                    }
+                }
+                if adjacent < 4 {
+                    to_remove.push((x, y));
+                }
             }
         }
 
@@ -38,19 +51,20 @@ impl Day04 {
     }
 
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
-        Ok(self.can_remove().len().into())
+        Ok(self.can_remove(Vec::new()).len().into())
     }
 
     fn part2(&mut self) -> Result<helper::RunOutput, Error> {
         let mut answer = 0;
+        let mut to_remove = Vec::new();
         loop {
-            let to_remove = self.can_remove();
+            to_remove = self.can_remove(to_remove);
             if to_remove.is_empty() {
                 break;
             }
             answer += to_remove.len();
-            for roll in to_remove.iter() {
-                self.rolls.remove(roll);
+            for (x, y) in to_remove.iter().copied() {
+                self.rolls[y][x] = false;
             }
         }
         Ok(answer.into())
@@ -60,13 +74,16 @@ impl Day04 {
 impl helper::Runner for Day04 {
     fn parse(&mut self, file: &[u8], _part: u8) -> Result<(), Error> {
         let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
-        for (y, line) in lines.iter().enumerate() {
-            for (x, c) in line.chars().enumerate() {
-                if c == '@' {
-                    self.rolls.insert((x as isize, y as isize));
-                }
-            }
+        for line in lines.iter() {
+            self.rolls.push(line.chars().map(|c| c == '@').collect());
         }
+        for rolls in self.rolls.iter_mut() {
+            rolls.insert(0, false);
+            rolls.push(false);
+        }
+        let empty = vec![false; self.rolls[0].len()];
+        self.rolls.insert(0, empty.clone());
+        self.rolls.push(empty);
         Ok(())
     }
 
